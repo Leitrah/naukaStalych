cc5060728aef05bffd6e09b08187e785b1820a6f
Mateusz Błaszak <mateusz.blaszak01999@gmail.com>
2018-07-25 19:03:47 +0200

    Dokonanie obliczen

--- constants.h
+++ constants.h
@@ -1,4 +1,10 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+//define my own namespace for constants
+namespace constants
+{
+    constexpr double pi(3.14159);
+    constexpr double gravity(9.8);
+}
 #endif // CONSTANS_H
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "constants.h"
 int main()
 {
-
+    double constatsMath = 2 * constants::pi;
+    std::cout << "Result: " << constatsMath << std::endl;
     return 0;
 }
