16b2861f41a7693c360ebbd61ea28024d34b16f4
Mateusz Błaszak <mateusz.blaszak01999@gmail.com>
2018-07-25 20:28:14 +0200

    dodanie stałej e

--- constants.h
+++ constants.h
@@ -4,7 +4,8 @@
 //define my own namespace for constants
 namespace constants
 {
-    constexpr double pi(3.14159);
-    constexpr double gravity(9.8);
+    constexpr double pi{3.14159};
+    constexpr double gravity{9.8};
+    constexpr double e{2.718281};
 }
 #endif // CONSTANS_H
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "constants.h"
 int main()
 {
-    double constatsMath = 2 * constants::pi;
+    double constatsMath = 2 * constants::pi * constants::e;
     std::cout << "Result: " << constatsMath << std::endl;
     return 0;
 }
